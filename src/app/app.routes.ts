b80928d9374fd43de9e94dead860e735665f6bb7
Gleidson <gleidsonrosasantos2@gmail.com>
2026-09-09 14:02:46 -0300

    cabeçario praticamente pronto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { TelaInicial } from './components/tela-inicial/tela-inicial';
+import { Sobre } from './components/sobre/sobre';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:"",
+        redirectTo: "tela-inicial",
+        pathMatch: "full",
+
+    },
+
+    { 
+        path: "tela-inicial",
+        component: TelaInicial },
+
+    { 
+        path: "sobre",
+        component: Sobre },
+
+    { 
+        path: "carrinho",
+        component: Sobre },
+        
+
+];
