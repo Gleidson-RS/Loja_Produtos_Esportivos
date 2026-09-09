404faad89ee846cf2b07325d6032cb43ead08368
Luiz Kauã <lkaua4546@gmail.com>
2026-09-09 14:16:36 -0300

    Criando as rotas para as páginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Sobre } from './components/sobre/sobre';
+import { TelaInicial } from './components/tela-inicial/tela-inicial';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+ {
+    path:'',
+    redirectTo:'/tela-inicial',
+    pathMatch:'full',
+ },
+ {
+    path:'tela-inicial',
+    component:TelaInicial,
+ },
+ {
+    path:'sobre',
+    component:Sobre
+ }
+];
