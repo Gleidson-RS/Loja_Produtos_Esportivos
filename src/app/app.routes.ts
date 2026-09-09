185e2662a699d945e8160ad2385207a464af1a63
Gleidson <gleidsonrosasantos2@gmail.com>
2026-09-09 15:06:47 -0300

    menu terminado 99%

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { TelaInicial } from './components/tela-inicial/tela-inicial';
 import { Sobre } from './components/sobre/sobre';
-//import { Carrinho } from './components/carrinho/carrinho';
 
 export const routes: Routes = [
 
@@ -22,6 +21,10 @@ export const routes: Routes = [
 
     { 
         path: "carrinho",
+        component: Sobre },
+
+    { 
+        path: "login",
         component: Sobre },
         
 
