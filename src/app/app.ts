61e0fac011444506c9c18744ba639a0eda9d758f
Gleidson <gleidsonrosasantos2@gmail.com>
2026-09-09 13:24:10 -0300

    processo do menu iniciado

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Menu } from './components/menu/menu';
 
 @Component({
-  imports: [RouterOutlet],
   selector: 'app-root',
-  styleUrl: './app.css',
+  imports: [
+  RouterOutlet, Menu],
   templateUrl: './app.html',
+  styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Loja_Produtos_Esportivos');
 }
